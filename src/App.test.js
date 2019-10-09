f474a08ee67f91815b2bd79453866c173fbdc85d
nick.durbin <nick.durbin@gmail.com>
2019-10-09 19:25:39 -0400

    Adding the changes to the test in app.

--- src/App.test.js
+++ src/App.test.js
@@ -5,13 +5,7 @@ import '@testing-library/jest-dom/extend-expect';
 afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
-<<<<<<< HEAD
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-=======
   const wrapper = rtl.render(
     <span className="greet">hello world</span>
   );
->>>>>>> 555bbcf45fcc5c70544bdaf2c20b7f1c93cfaaad
 });
