585f8ed0658d45bde98e174658c9c86e26e47d98
ndurbin <nick.durbin@gmail.com>
2019-10-09 16:21:34 -0400

    Created the boilerplate components and brought in the testing libraries.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
 
 function App() {
   return (
     <div className="App">
-     
+      <Dashboard />
+      <Display />
     </div>
   );
 }
