6080abd023236c24ac9416ffd783a5e70793d624
nick.durbin <nick.durbin@gmail.com>
2019-10-10 17:46:49 -0400

    Added more buttons to make a full scoreboard.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Display from './components/Display';
 function App() {
   const [balls, setBalls] = useState(0);
   const [strikes, setStrikes] = useState(0);
+  const [innings, setInnings] = useState(0);
+  const [errors, setErrors] = useState(0);
+  const [outs, setOuts] = useState(0);
+  const [hits, setHits] = useState(0);
 
   return (
     <div className="App">
       <header data-testid="main-header" className="App-header">
-        <p> Baseball Application </p>
-        <Display balls={balls} strikes={strikes}/>
-        <Dashboard balls={balls} strikes={strikes} setBalls={setBalls} setStrikes={setStrikes} />
+        <p> Scoreboard </p>
+        <Display balls={balls} strikes={strikes} errors={errors} innings={innings} outs={outs} hits={hits} />
+        <Dashboard balls={balls} strikes={strikes} setBalls={setBalls} setStrikes={setStrikes} innings={innings} errors={errors} outs={outs} setOuts={setOuts} setInnings={setInnings} setErrors={setErrors} hits={hits} setHits={setHits} />
       </header>
     </div>
   );
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 function Display(props) {
-  const {balls, strikes} = props;
+  const {balls, strikes, outs, innings, errors, hits} = props;
 
   return (
-     <div className="atBatContainer">
+     <div className="gameContainer">
         <div className="strikesContainer">
            <h2 className="strikesHeader">Strikes</h2>
            <div className="strikesDisplay">{strikes}</div>
@@ -13,6 +13,26 @@ function Display(props) {
         <div className="ballsContainer">
            <h2 className="ballsHeader">Balls</h2>
            <div className="ballsDisplay">{balls}</div>
+        </div>
+
+        <div className="outsContainer">
+           <h2 className="outsHeader">Outs</h2>
+           <div className="outsDisplay">{outs}</div>
+        </div>
+
+        <div className="hitsContainer">
+           <h2 className="hitsHeader">Hits</h2>
+           <div className="hitsDisplay">{hits}</div>
+        </div>
+
+        <div className="inningsContainer">
+           <h2 className="inningsHeader">Inning</h2>
+           <div className="inningsDisplay">{innings}</div>
+        </div>
+
+        <div className="errorsContainer">
+           <h2 className="errorsHeader">Errors</h2>
+           <div className="errorsDisplay">{errors}</div>
         </div>
      </div>
   );
